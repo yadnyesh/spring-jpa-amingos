d50c027ba81f944e814db6129b76fccb0599564e
yadnyesh <yadnyesh.juvekar@gmail.com>
2021-01-26 18:49:53 +0530

    First Commit

---
+++ src/main/java/io/yadnyesh/springjpaamingos/SpringJpaAmingosApplication.java
@@ -0,0 +1,13 @@
+package io.yadnyesh.springjpaamingos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaAmingosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaAmingosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/yadnyesh/springjpaamingos/SpringJpaAmingosApplicationTests.java
@@ -0,0 +1,13 @@
+package io.yadnyesh.springjpaamingos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaAmingosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
